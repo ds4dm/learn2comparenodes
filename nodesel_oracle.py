2996c592c5be0b9b0ef36adfd85af25ccdc36ae7
Maxime Gasse <maxime.gasse@gmail.com>
2020-05-25 16:03:40 -0400

    Oracle implementation

--- nodesel_oracle.py
+++ nodesel_oracle.py
@@ -3,189 +3,158 @@ from pyscipopt import Model, Nodesel, Eventhdlr
 import pyscipopt
 import argparse
 
-class Default(Nodesel):
+class InternalNodeSelector(Nodesel):
+    def __init__(self, nodesel_name):
+        self.nodesel_name = nodesel_name
+
     def nodeselect(self):
-        node = self.model.executeNodeSel('estimate')
-        return {"selnode": node}
+        selnode = self.model.executeNodeSel(self.nodesel_name)
+        return {"selnode": selnode}
 
-
-class Oracle(Nodesel):
-    def __init__(self, solution_path): 
-        self.opt_checked = {} 
-        self.opt_nodes = {}
-        self.feat = {'maxdepth':[]}
-        self.optfeat = {'maxdepth':[]}
-        self.optnodenumber = -1
-        self.negate = True
-        self.solution_path = solution_path
-        
-    def nodeinit(self): 
-        self.feat['maxdepth'].append(self.model.getNBinVars() + self.model.getNintVars())
-        self.optfeat['maxdepth'].append(self.model.getNBinVars() + self.model.getNintVars())
-        self.opt_sol = self.model.readSolFile(self.solution_path)
-
-    def nodeselect(self): 
-        leaves, children, siblings = self.model.getOpenNodes()
-        # print(type(leaves), type(children), type(siblings))
-        self.optchild = -1
-        for idx, child in enumerate(children):
-            # print("loop activated")
-            if child.getNumber() not in self.opt_checked: 
-                parent = child.getParent()
-                # root -> trivially optimal 
-                if(parent.getDepth() > 0 and not self.opt_nodes[parent.getNumber()]): 
-                    optimal = True   
-                nbranchvars = child.getNParentBranchings()
-                # print(nbranchvars)
-                branchvars, branchbounds, boundtypes = child.getParentBranchings()
-                optimal = True 
-                for i in range(nbranchvars):
-                    optval = self.model.getSolVal(self.opt_sol, branchvars[i])
-                    # print(optval)
-                    if boundtypes[i] == 0 and optval < branchbounds[i] or boundtypes[i] == 1 and optval > branchbounds[i]: 
-                        optimal = False 
-                self.opt_checked[child.getNumber()] = True
-                self.opt_nodes[child.getNumber()] = optimal 
-            
-            if child.getNumber() in self.opt_nodes:
-                self.optnodenumber = child.getNumber()
-                self.optchild = idx          
-                # print(self.optnodenumber)
-                # print(self.optchild)
-
-            #TODO: Lines 461 through to 503 of the nodesel_oracle.c file need to implemented here. 
-
-        node = self.model.getBestNode()
-        return {"selnode": node}
-        
     def nodecomp(self, node1, node2): 
-        isoptimal1 = self.opt_nodes[node1.getNumber()]
-        isoptimal2 = self.opt_nodes[node2.getNumber()]
-        if(isoptimal1):
-            assert(not isoptimal2)
-            return -1
-        elif isoptimal2:
-            return 1
-        else:
-            depth1 = node1.getDepth()
-            depth2 = node2.getDepth()
-            if (depth1 > depth2):
-                return -1
-            elif (depth1 < depth2):
-                return 1
-            else:
-                lowerbound1 = node1.getLowerbound()
-                lowerbound2 = node2.getLowerbound()
-                if(self.model.isLT(lowerbound1, lowerbound2)):
-                    return -1
-                elif(self.model.isGT(lowerbound1, lowerbound2)):
-                    return 1
-                # Why check the same thing twice? What is the purpose/benefits?
-                if lowerbound1 < lowerbound2:
-                    return -1
-                elif lowerbound1 > lowerbound2:
-                    return 1
-                else:
-                    return 0
+        return self.model.executeNodeComp(self.nodesel_name, node1, node2)
 
 
-class OracleNodeSel(Nodesel):
-    def __init__(self, solution_path): 
-        self.opt_checked = {} 
-        self.opt_nodes = {}
-        self.feat = {'maxdepth':[]}
-        self.optfeat = {'maxdepth':[]}
-        self.optnodenumber = -1
-        self.negate = True 
-        self.solution_path = solution_path       
+class OracleNodeSelector(Nodesel):
+    def __init__(self, optsol, nodesel_name):
+        self.nodesel_name = nodesel_name
+        self.optsol = optsol
+        self.optnode = None
 
-    def nodeinit(self): 
-        self.feat['maxdepth'].append(self.model.getNBinVars() + self.model.getNintVars())
-        self.optfeat['maxdepth'].append(self.model.getNBinVars() + self.model.getNintVars())
-        self.opt_sol = self.model.readSolFile(self.solution_path)
+    def nodeinitsol(self): 
+        # TODO: how does nodeinitsol handle restarts ?
+        self.optnode = self.model.getRootNode()
+        self.optnode_selected = False
 
-    
-    def nodeselect(self): 
+    def nodeselect(self):
+        selnode = None
+
         leaves, children, siblings = self.model.getOpenNodes()
-        # print(type(leaves), type(children), type(siblings))
-        self.optchild = -1
-        for idx, child in enumerate(children):
-            # print("loop activated")
-            if child.getNumber() not in self.opt_checked: 
-                parent = child.getParent()
-                # root -> trivially optimal 
-                if(parent.getDepth() > 0 and not self.opt_nodes[parent.getNumber()]): 
-                    optimal = True   
-                nbranchvars = child.getNParentBranchings()
-                # print(nbranchvars)
-                branchvars, branchbounds, boundtypes = child.getParentBranchings()
-                optimal = True 
-                for i in range(nbranchvars):
-                    optval = self.model.getSolVal(self.opt_sol, branchvars[i])
-                    # print(optval)
-                    if boundtypes[i] == 0 and optval < branchbounds[i] or boundtypes[i] == 1 and optval > branchbounds[i]: 
-                        optimal = False 
-                self.opt_checked[child.getNumber()] = True
-                self.opt_nodes[child.getNumber()] = optimal 
-            
-            if child.getNumber() in self.opt_nodes:
-                self.optnodenumber = child.getNumber()
-                self.optchild = idx
+        print(f"    oracle: {len(leaves)} leaves, {len(children)} children, {len(siblings)} siblings")
 
-            #TODO: Lines 461 through to 503 of the nodesel_oracle.c file need to implemented here. 
+        # if optimal node was selected last, update it
+        if self.optnode_selected:
+            print(f"    oracle: updating optimal node")
+            # if it has children, one of those is the new optimal node
+            if children:
+                for child in children:
+                    # check whether the child node contains the optimal solution
+                    childisopt = True
+                    vars, bounds, btypes = child.getParentBranchings()
+                    for var, bound, btype in zip(vars, bounds, btypes):
+                        optval = self.model.getSolVal(self.optsol, var)
+                        # lower bound
+                        if btype == 0 and self.model.isLT(optval, bound):
+                            childisopt = False
+                            break
+                        # upper bound
+                        if btype == 1 and self.model.isGT(optval, bound): 
+                            childisopt = False
+                            break
+                    # when optimal child is found, stop
+                    if childisopt:
+                        break
+                # assert one child is optimal
+                assert childisopt
+                self.optnode = child
+            # else there is no optimal node any more
+            else:
+                self.optnode = None
+                print(f"    oracle: no more optimal node")
 
-        node = self.model.getBestNode()
-        return {"selnode": node}
+        if self.optnode:
+            selnode = self.optnode
+            print(f"    oracle: selecting the optimal node")
+        else:
+            selnode = self.model.executeNodeSel(self.nodesel_name)
+            print(f"    oracle: selecting the '{self.nodesel_name}' node")
+
+        # checks whether the selected node is the optimal one
+        self.optnode_selected = (self.optnode and self.optnode == selnode)
+
+        if selnode:
+            print(f"    selected node {selnode.getNumber()}")
+        else:
+            print(f"    no node selected")
+
+        return {"selnode": selnode}
+
+    def nodecomp(self, node1, node2): 
+        return self.model.executeNodeComp(self.nodesel_name, node1, node2)
 
 
-# loop over multiple problems 
-parser = argparse.ArgumentParser(description='Solve a few MILPs')
-parser.add_argument('method')
-args = parser.parse_args()
+instances = [f"data/instances/indset/transfer_500_4/instance_{i+1}.lp" for i in range(5)]
+# instances = Path('data/instances/indset/transfer_500_4/').glob("*.lp")
+# instances = Path('data/instances/setcover/transfer_500r_1000c_0.05d/').glob("*.lp")
+# instances = Path('/Users/work/Desktop/Learn2SelectNodes/mik.data/bounded/').rglob('*.mps.gz')    # 90 bounded files
 
-list_files_indset = Path('/Users/work/Desktop/learn2branch/data/instances/indset/transfer_500_4/').glob("*.lp")
-for problem_path in list_files_indset: 
+instances = [str(instance) for instance in instances]
+
+instances = instances[1:2]
+
+nodeselectors = [
+    'estimate',
+    # 'bfs',
+    # 'hybridestim',
+    # 'restartdfs',
+    # 'uct',
+    # 'dfs',
+    # 'breadthfirst',
+]
+
+
+for instance in instances: 
+    print(f"instance {instance}")
+    sol_file = instance.strip(".") + "_solution.txt"
     m = Model()
-    m.readProblem(str(problem_path))
-    if args.method == 'oracle': 
-        m.includeNodesel(Oracle(str(problem_path) + '_solution.txt'), "oracle_nodesel", "", 1073741823, 536870911)
-    if args.method == 'oracle_nodesel': 
-        m.includeNodesel(OracleNodeSel(str(problem_path) + '_solution.txt'), "oracle_nodesel_only", "", 1073741823, 536870911)
-    if args.method == 'default': 
-        m.includeNodesel(Default(), "default", "", 1073741823, 536870911)
+    m.hideOutput()
+    m.readProblem(instance)
     m.optimize()
-    m.writeStatistics((str(problem_path) + str(args.method)).strip(".") + "stats.txt")
+    m.writeBestSol(sol_file)
     m.freeProb()
+    print(f"optimal solution written to {sol_file}")
 
-list_files_setcover = Path('/Users/work/Desktop/learn2branch/data/instances/setcover/transfer_500r_1000c_0.05d/').glob("*.lp")
-for problem_path in list_files_setcover: 
-    m = Model()
-    m.readProblem(str(problem_path))
-    if args.method == 'oracle': 
-        m.includeNodesel(Oracle(str(problem_path) + '_solution.txt'), "oracle_nodesel", "", 1073741823, 536870911)
-    if args.method == 'oracle_nodesel': 
-        m.includeNodesel(OracleNodeSel(str(problem_path) + '_solution.txt'), "oracle_nodesel_only", "", 1073741823, 536870911)
-    if args.method == 'default': 
-        m.includeNodesel(Default(), "default", "", 1073741823, 536870911)
-    m.optimize()
-    m.writeStatistics((str(problem_path) + str(args.method)).strip(".") + "stats.txt")
-    m.freeProb()
-    
+    for nodeselector in nodeselectors:
 
-list_files_bounded = Path('/Users/work/Desktop/Learn2SelectNodes/mik.data/bounded/').rglob('*.mps.gz')    # 90 bounded files
-for path in list_files_bounded:
-    m = Model()
-    m.readProblem(str(path))
-    if args.method == 'oracle': 
-        m.includeNodesel(Oracle(str(path) + '_solution.txt'), "oracle_nodesel", "", 1073741823, 536870911)
-    if args.method == 'oracle_nodesel': 
-        m.includeNodesel(OracleNodeSel(str(path) + '_solution.txt'), "oracle_nodesel_only", "", 1073741823, 536870911)
-    if args.method == 'default': 
-        m.includeNodesel(Default(), "default", "", 1073741823, 536870911)
-    m.optimize()
-    m.writeStatistics((str(path) + str(args.method)).strip(".") + "stats.txt")
-    m.freeProb()
-    
-    
+        # Oracle
+        m = Model()
+        m.hideOutput()
+        m.readProblem(instance)
+        solution = m.readSolFile(sol_file)
+        m.includeNodesel(OracleNodeSelector(solution, nodeselector), f"py_{nodeselector}", "", 666666, 666666)
+        m.optimize()
+        nnodes = m.getNNodes()
+        nlpiterations = m.getNLPIterations()
+        stime = m.getSolvingTime()
+        pdi = m.getStatPrimalDualIntegral()
+        print(f"  oracle_{nodeselector}: {nnodes} nodes, {stime} solving time, {pdi} PDI")
+        m.freeProb()
+        del solution
 
+        # vanilla SCIP rules
+        m = Model()
+        m.hideOutput()
+        m.readProblem(instance)
+        m.setParam(f"nodeselection/{nodeselector}/stdpriority", 666666)
+        m.setParam(f"nodeselection/{nodeselector}/memsavepriority", 666666)
+        m.optimize()
+        nnodes = m.getNNodes()
+        nlpiterations = m.getNLPIterations()
+        stime = m.getSolvingTime()
+        pdi = m.getStatPrimalDualIntegral()
+        print(f"  {nodeselector}: {nnodes} nodes, {stime} solving time, {pdi} PDI")
+        m.freeProb()
+
+        # # python SCIP rules
+        # m = Model()
+        # m.hideOutput()
+        # m.readProblem(instance)
+        # m.includeNodesel(InternalNodeSelector(nodeselector), f"py_{nodeselector}", "", 666666, 666666)
+        # m.optimize()
+        # nnodes = m.getNNodes()
+        # nlpiterations = m.getNLPIterations()
+        # stime = m.getSolvingTime()
+        # pdi = m.getStatPrimalDualIntegral()
+        # print(f"  py_{nodeselector}: {nnodes} nodes, {stime} solving time, {pdi} PID")
+        # m.freeProb()
 
