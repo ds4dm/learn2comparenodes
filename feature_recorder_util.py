9db071a4225f300c576a4201c26136b92a7f4dea
cromagnonninja <bhanubhandari99@gmail.com>
2020-06-22 10:12:58 -0400

    All features successfully extracted

--- feature_recorder_util.py
+++ feature_recorder_util.py
@@ -3,28 +3,40 @@ import pyscipopt
 from pyscipopt import Nodesel, Eventhdlr 
 import numpy as np
 
-class NodeFocusHandler(pyscipopt.Eventhdlr):
-    def __init__(self):        
-        self.stats = []
-        # self.return_ = []
-        # self.nb_nodes = []
-        # self.nb_lp_iterations = []
-        # self.solving_time = []
-        self.node_lb = []
-        self.depth = []
-        self.estimate = []
-        self.node_type = []
-        self.ub = []
-        self.lb = []
-        self.lb_root = []
-        self.branchbound = []
-        self.branchvar = []
-        self.plungedepth = []
-        self.branchvar = []
-        self.branchbound = []
-        self.branchdir = []
-        self.varsol = []
 
+class NodeFocusHandler(pyscipopt.Eventhdlr):
+    def __init__(self):
+        self.node_lb = 0
+        self.depth = 0 
+        '''
+        These features below are the node features. 
+        '''
+        self.nodeFeatures = []
+        self.nodeselGap = 0 
+        self.nodeselGapInf = 0 
+        self.relativeDepth = 0
+        self.lowerBound = 0 
+        self.estimate = 0
+        self.relativeBound = 0
+        self.globalUpperBound = 0
+        self.globalUpperBoundInf = 0
+        self.plungeDepth = 0  
+        self.nodeIsSibling = 0 
+        self.nodeIsChild = 0 
+        self.nodeIsLeaf = 0 
+        '''
+        These features below are the features obtained according to the branching 
+        status at the time of a particular node being focussed. 
+        '''
+        self.branchFeatures = []
+        self.boundLPDiff = 0 
+        self.rootLPDiff = 0
+        self.pseudocost = 0 
+        self.branchPriorityDown = 0
+        self.branchPriorityUp = 0
+        self.branchVarInf = 0
+
+        
     def eventinit(self):
         self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEFOCUSED, self)
 
@@ -32,20 +44,102 @@ class NodeFocusHandler(pyscipopt.Eventhdlr):
         self.model.dropEvent(pyscipopt.SCIP_EVENTTYPE.NODEFOCUSED, self)
 
     def eventexec(self, event):
-        self.node_lb.append(self.model.getCurrentNode().getLowerbound())
-        self.depth.append(self.model.getDepth())
-        self.estimate.append(self.model.getCurrentNode().getEstimate())
-        self.node_type.append(self.model.getCurrentNode().getType())
-        self.ub.append(self.model.getUpperbound())
-        self.lb.append(self.model.getLowerbound())
-        self.lb_root.append(self.model.getRootLowerBound())
-        self.plungedepth.append(self.model.getPlungeDepth())
-        if self.model.getCurrentNode().getDomchg():
-            self.branchbound.append(self.model.getCurrentNode().getDomchg().getBoundchgs()[0].getNewBound())
-            self.branchvar.append(self.model.getCurrentNode().getDomchg().getBoundchgs()[0].getVar())
-            self.branchdir.append(self.model.getCurrentNode().getDomchg().getBoundchgs()[0].getVar().getVarBranchDir())
-            self.varsol.append(self.model.getCurrentNode().getDomchg().getBoundchgs()[0].getVar().getLPSol())
+        # Node features
+        if self.model:     
+            self.maxdepth = self.model.getNBinVars() + self.model.getNintVars()
+        else: 
+            self.maxdepth = 0
+        self.depth = self.model.getDepth()
+        if self.depth > self.maxdepth: 
+            self.maxdepth = self.depth
+        lb_root = abs(self.model.getRootLowerBound())
+        lb_node = self.model.getCurrentNode().getLowerbound()
+        ub = self.model.getUpperbound()
+        lb = self.model.getLowerbound()
 
+        if lb_root == 0: 
+            lb_root = 0.0001
+        
+        if lb == ub: 
+            self.nodeselGap = 1
+        elif abs(ub) >= 1e+20 or lb == 0: 
+            self.nodeselGapInf = 1
+        else: 
+            self.nodeselGap = (ub - lb) / abs(lb)
+        
+        if abs(ub) >= 1e20: 
+            self.globalUpperBoundInf = 1 
+            ub = lb + 0.2 * (ub - lb)
+        else: 
+            self.globalUpperBound = ub / lb_root
+        
+        self.relativeDepth = self.depth / (self.maxdepth * 10)
+        self.lowerBound = lb / lb_root 
+        estimate = self.model.getCurrentNode().getEstimate()
+        self.estimate = estimate / lb_root
+        if lb != ub: 
+            self.relativeBound = (lb_node - lb) / (ub - lb)
+        
+        self.plungeDepth = self.model.getPlungeDepth()
+        
+        node_type = self.model.getCurrentNode().getType()
+        if node_type == 2: 
+            self.nodeIsSibling = 1 
+        elif node_type == 3: 
+            self.nodeIsChild = 1 
+        elif node_type == 4: 
+            self.nodeIsLeaf = 1
+
+        self.nodeFeatures.append([self.nodeselGap, 
+                        self.nodeselGapInf, 
+                        self.relativeDepth, 
+                        self.lowerBound, 
+                        self.estimate,
+                        self.globalUpperBound, 
+                        self.globalUpperBoundInf,
+                        self.relativeBound, 
+                        self.plungeDepth, 
+                        self.nodeIsSibling, 
+                        self.nodeIsChild, 
+                        self.nodeIsLeaf 
+                        ])
+
+        # Branching features 
+        domainChange = self.model.getCurrentNode().getDomchg()
+        if domainChange:
+            branchbound = domainChange.getBoundchgs()[0].getNewBound()
+            branchvar =  domainChange.getBoundchgs()[0].getVar()
+            branchdir =  branchvar.getVarBranchDir()
+            # This is broken, it needs some salt and pepper SCIP_TREE stuff to solve this properly 
+            haslp = self.model.focusNodeHasLP()
+            if haslp == 1: 
+                varsol = branchvar.getPseudoSol()
+            else: 
+                varsol =  branchvar.getLPSol()
+            varrootsol =  branchvar.getRootSol()
+            boundtype =  domainChange.getBoundchgs()[0].getBoundtype()
+
+            self.boundLPDiff = branchbound - varsol
+            self.rootLPDiff = varrootsol - varsol
+            self.pseudocost = self.model.getPseudoCost(branchvar, branchbound - varsol)
+            if branchdir == 1:
+                self.branchPriorityDown = 1 
+            elif branchdir == 0: 
+                self.branchPriorityUp = 1     
+            
+            if boundtype == 1: 
+                self.branchVarInf = self.model.getAvgInferences(branchvar, 1) / self.maxdepth
+            else: 
+                self.branchVarInf = self.model.getAvgInferences(branchvar, 0) / self.maxdepth
+        
+            self.branchFeatures.append([
+                self.boundLPDiff, 
+                self.rootLPDiff, 
+                self.pseudocost, 
+                self.branchPriorityDown, 
+                self.branchPriorityUp, 
+                self.branchVarInf
+            ])
 
 m = Model()
 m.readProblem("/Users/work/Desktop/Learn2SelectNodes/er_n=74_m=1338_p=0.50_SET2_setparam=100.00_alpha=0.75_0.lp")
@@ -53,16 +147,5 @@ node_recorder = NodeFocusHandler()
 m.includeEventhdlr(node_recorder, "NodeFocusHandler", "")
 m.hideOutput()
 m.optimize()
-print(len(node_recorder.node_lb[1:]))
-print(node_recorder.depth[:5])
-print(node_recorder.estimate[:5])
-print(node_recorder.node_type[:5])
-print(node_recorder.lb[:5])
-print(node_recorder.ub[:5])
-print(node_recorder.lb_root[:5])
-print(node_recorder.plungedepth[:5])
-print(len(node_recorder.branchbound))
-print(node_recorder.branchvar[:5])
-print(node_recorder.branchdir[:5])
-print(node_recorder.varsol[:5])
+print(node_recorder.nodeFeatures[:5], node_recorder.branchFeatures[:5])
 m.freeProb()
