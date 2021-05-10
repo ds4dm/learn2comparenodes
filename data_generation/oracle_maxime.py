c3e1b4cb3a7ad15db3d50d905e3c5faa77ce4dfe
Abdel <abdel.ghani.labassi@gmail.com>
2021-05-10 11:42:18 -0400

    added node basic node selectors with maxime oracle

--- data_generation/oracle_maxime.py
+++ data_generation/oracle_maxime.py
@@ -5,6 +5,45 @@ import argparse
 from feature_recorder_util import NodeFeatureRecorder
 import csv
 
+class Default_Estimate(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('estimate')
+        return {"selnode": node}
+    def nodecomp(self, node1, node2): 
+        node = self.model.executeNodeComp('estimate', node1, node2)
+
+
+class Default_BFS(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('bfs')
+        return {"selnode": node}
+
+class Default_Breadth(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('breadthfirst')
+        return {"selnode": node}
+
+class Default_DFS(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('dfs')
+        return {"selnode": node}
+
+class Default_Hybrid(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('hybridestim')
+        return {"selnode": node}
+
+class Default_UCT(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('uct')
+        return {"selnode": node}
+
+class Default_Restart(Nodesel):
+    def nodeselect(self):
+        node = self.model.executeNodeSel('restartdfs')
+        return {"selnode": node}
+
+
 
 class InternalNodeSelector(Nodesel):
     def __init__(self, nodesel_name):
@@ -17,7 +56,7 @@ class InternalNodeSelector(Nodesel):
     def nodecomp(self, node1, node2): 
         return self.model.executeNodeComp(self.nodesel_name, node1, node2)
 
-
+#Maxime
 class OracleNodeSelector(Nodesel):
     def __init__(self, optsol, nodesel_name, instance):
         self.nodesel_name = nodesel_name
