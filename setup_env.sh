0f32ab537edb1f8d58ef5352e6e82d6ae7405cbe
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2021-11-15 12:24:16 -0500

    Update env.yml and setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,25 @@
 conda init bash
 #conda update -n base -c defaults conda
 
+source deactivate 
+
 conda env remove -n l2sn 
 
 conda env create -f env.yml
 
-conda install pyg -c pyg -c conda-forge -n l2sn
-
 source activate l2sn
+
 pip install ./pyscipopt
+
+#CPU
+#pip install torch==1.10.0+cpu torchvision==0.11.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+#pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
+
+#GPU
+pip install torch torchvision torchaudio
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cu102.html
+
+
+
+#cuda=10.2
 
