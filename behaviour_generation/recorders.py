69d111302a52715da01a02664ef39b50e1c1ba31
Abdel Ghani Labassi <abdel.ghani.labassi@umontreal.ca>
2021-11-22 19:32:40 -0500

    Update recorders.py

--- behaviour_generation/recorders.py
+++ behaviour_generation/recorders.py
@@ -65,11 +65,9 @@ class CompBehaviourSaver():
         edge_indices = torch.LongTensor(g_data[2])
         edge_features = torch.FloatTensor(g_data[3])
         y = g_data[4]
-        num_nodes = variable_features.size(0) + constraint_features.size(0)
         
         data = BipartiteNodeData(variable_features, constraint_features, edge_indices,
                                  edge_features, y)
-        data.num_nodes = num_nodes
         
         torch.save(data, path)
         
