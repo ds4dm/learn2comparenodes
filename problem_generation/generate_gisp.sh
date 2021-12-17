c595d9298846de91a9a4fe1e40acc0af740a7f4b
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2021-12-16 22:20:16 -0500

    Update generate_gisp.sh :uncommented stuff

--- problem_generation/generate_gisp.sh
+++ problem_generation/generate_gisp.sh
@@ -1,4 +1,4 @@
 source activate l2sn
-#python gisp_generator.py -min_n 50 -max_n 60 -exp_dir data/GISP/train -SolveInstance 1 -n_instance 10000 
+python gisp_generator.py -min_n 50 -max_n 60 -exp_dir data/GISP/train -SolveInstance 1 -n_instance 10000 
 python gisp_generator.py -min_n 50 -max_n 60 -exp_dir data/GISP/valid -SolveInstance 1 -n_instance 1000
 python gisp_generator.py -min_n 50 -max_n 60 -exp_dir data/GISP/test -SolveInstance 0  -n_instance 1000
