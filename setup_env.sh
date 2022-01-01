529606fdba40fba4bb1d70fed4305c7483b2ab23
Abdel Ghani Labassi <abdel.ghani.labassi@umontreal.ca>
2021-12-31 22:47:47 -0500

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -15,10 +15,10 @@ pip install torch torchvision torchaudio
 #pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
 
 #GPU
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu102.html
-pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.1+cu102.html
-pip install torch-cluster -f https://data.pyg.org/whl/torch-1.10.1+cu102.html
-pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.10.1+cu102.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
 pip install torch-geometric
 
 
