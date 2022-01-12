4aa986254d60712b5c7bedc673dc934f151f0d4d
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2022-01-11 21:25:39 -0500

    Update setup_env.sh : tryind toch 10.9

--- setup_env.sh
+++ setup_env.sh
@@ -8,17 +8,17 @@ conda env create -f env.yml
 conda activate l2sn
 
 pip install ./pyscipopt
-pip install torch==1.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+pip install torch==1.9.0+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
 
 #CPU
 
 #pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
 
 #GPU
-pip install --no-index torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
-pip install --no-index torch-sparse -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
-pip install --no-index torch-cluster -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
-pip install --no-index torch-spline-conv -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+pip install --no-index torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
+pip install --no-index torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
+pip install --no-index torch-cluster -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
+pip install --no-index torch-spline-conv -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
 pip install torch-geometric
 
 
