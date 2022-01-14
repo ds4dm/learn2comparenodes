03798a4e7aa7d4f69b21c0bf54aba7c77c9ebec4
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2022-01-14 13:03:28 -0500

    Update train.py : full normalizer

--- learning/train.py
+++ learning/train.py
@@ -44,42 +44,42 @@ def normalize_graph(constraint_features,
     #     #normalize right hand side
     #     constraint_features[c] /= normalizer
     
-    # vars_to_normalize = torch.where( torch.max(torch.abs(variable_features[:, :2]), axis=1)[0] > 1)[0]
+    vars_to_normalize = torch.where( torch.max(torch.abs(variable_features[:, :2]), axis=1)[0] > 1)[0]
 
-    # coeffs = torch.max(torch.abs(variable_features[vars_to_normalize, :2]) , axis=1)[0]
+    coeffs = torch.max(torch.abs(variable_features[vars_to_normalize, :2]) , axis=1)[0]
     
-    # for v, cf in zip(vars_to_normalize, coeffs):
+    for v, cf in zip(vars_to_normalize, coeffs):
      
-    #  #normaize feature bound
-    #  variable_features[ v, :2] = variable_features[ v, :2]/cf
+      #normaize feature bound
+      variable_features[ v, :2] = variable_features[ v, :2]/cf
      
-    #  #update obj coeff and associated edges
-    #  variable_features[ v, 2 ] = variable_features[ v, 2 ]*cf 
+      #update obj coeff and associated edges
+      variable_features[ v, 2 ] = variable_features[ v, 2 ]*cf 
      
-    #  associated_edges = torch.where(edge_index[0] == v)[0]
-    #  edge_attr[associated_edges] = edge_attr[associated_edges]*cf
+      associated_edges = torch.where(edge_index[0] == v)[0]
+      edge_attr[associated_edges] = edge_attr[associated_edges]*cf
     
         
-    # #Normalize constraints 
-    # for c in range(constraint_features.shape[0]):
+    #Normalize constraints 
+    for c in range(constraint_features.shape[0]):
         
-    #     associated_edges =  torch.where(edge_index[1] == c)[0]
-    #     normalizer = max(torch.max(torch.abs(edge_attr[associated_edges]), axis=0)[0], 
-    #                      torch.abs(constraint_features[c]))
+        associated_edges =  torch.where(edge_index[1] == c)[0]
+        normalizer = max(torch.max(torch.abs(edge_attr[associated_edges]), axis=0)[0], 
+                          torch.abs(constraint_features[c]))
         
-    #     #normalize associated edges
-    #     edge_attr[associated_edges] = edge_attr[associated_edges] / normalizer
+        #normalize associated edges
+        edge_attr[associated_edges] = edge_attr[associated_edges] / normalizer
         
-    #     #normalize right hand side
-    #     constraint_features[c] = constraint_features[c] / normalizer
+        #normalize right hand side
+        constraint_features[c] = constraint_features[c] / normalizer
     
-    # #normalize objective
-    # normalizer = torch.max(torch.abs(variable_features[:,2]), axis=0)[0]
-    # variable_features[:,2] = variable_features[:,2] / normalizer
+    #normalize objective
+    normalizer = torch.max(torch.abs(variable_features[:,2]), axis=0)[0]
+    variable_features[:,2] = variable_features[:,2] / normalizer
     
-    constraint_features /= 300.0
-    variable_features[:3] /= 300.0
-    edge_attr /= 300.0
+    # constraint_features /= 300.0
+    # variable_features[:3] /= 300.0
+    # edge_attr /= 300.0
     
     return (constraint_features, edge_index, edge_attr, variable_features)
 
@@ -141,8 +141,8 @@ def process(policy, data_loader, loss_fct, optimizer=None, balance=True):
 
 
 problems = ["GISP"]
-LEARNING_RATE = 0.1
-NB_EPOCHS = 50
+LEARNING_RATE = 0.01
+NB_EPOCHS = 30
 PATIENCE = 10
 EARLY_STOPPING = 20
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
