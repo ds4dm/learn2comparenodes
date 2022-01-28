98168bf1c68a7923c79bf3968d8ec3fb4b3688d9
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2022-01-28 16:02:20 -0500

    Update utils.py : major code cleaning

--- utils.py
+++ utils.py
@@ -8,6 +8,21 @@ Created on Fri Jan 28 12:36:43 2022
 
 import pyscipopt.scip as sp
 import sys
+from node_selection.recorders import CompFeaturizer, LPFeatureRecorder
+from node_selection.node_selectors import CustomNodeSelector,OracleNodeSelectorEstimator, OracleNodeSelectorAbdel
+from learning.train import normalize_graph
+import re
+
+
+def setup_oracles(model, optsol, oracles):
+    for o in oracles:
+        o.setOptsol(optsol)
+        try:
+            o.set_LP_feature_recorder(LPFeatureRecorder(model.getVars(), model.getConss()))
+        except AttributeError: #oracle Abdel has no lp feature recorder
+            ''
+        
+
 
 #take a list of nodeselectors to evaluate, a list of instance to test on, and the 
 #problem type for printing purposes
@@ -19,68 +34,35 @@ def record_stats(nodesels, instances, problem, normalize=True, device='cpu', ver
     model.setIntParam('randomization/permutationseed', 9)
     model.setIntParam('randomization/randomseedshift',9)
     
-    oracle_estimator_trained = None
-    oracle_estimator_untrained = None
-    oracle_0 = None
-    oracle = None
-    
-    #creating random nodesel
-    if "random" in nodesels:
-        from node_selection.node_selectors.classic_selectors import Random
-        random = Random(record_fpath='decisions_rand.csv')
-        model.includeNodesel(random, "random", 'testing',100, 100)
-    if "estimate_custom" in nodesels:
-        from node_selection.node_selectors.classic_selectors import EstimateComp
-        comp = EstimateComp()
-        model.includeNodesel(comp, "estimate_custom", 'testing',100, 100)
+    oracles = []
+    for nodesel in nodesels :
         
-    #creating oracle estimator nodesels
-    if "gnn_trained" in nodesels:
-        from node_selection.recorders import CompFeaturizer, LPFeatureRecorder
-        from node_selection.node_selectors.oracle_selectors import OracleNodeSelectorEstimator
-        from learning.train import normalize_graph
+        if re.match('custom_*', nodesel):
+            name = nodesel.split("_")[-1]
+            comp = CustomNodeSelector(name)
+            
+        elif nodesel in ['gnn_trained', 'gnn_untrained']:
+            trained = nodesel.split('_')[-1] == "trained"            
+            comp_featurizer = CompFeaturizer(normalizor=normalize_graph if normalize else None)
+            comp = OracleNodeSelectorEstimator(problem,
+                                               comp_featurizer,
+                                               DEVICE=device,
+                                               record_fpath=f"decisions_{nodesel}.csv",
+                                               use_trained_gnn=trained)
+            oracles.append(comp)
         
-        comp_featurizer = CompFeaturizer(normalizor=normalize_graph if normalize else None)
-        oracle_estimator_trained = OracleNodeSelectorEstimator(problem,
-                                                       comp_featurizer,
-                                                       DEVICE=device,
-                                                       record_fpath="decisions_gnn_trained.csv",
-                                                       use_trained_gnn=True)
+        elif re.match('oracle*', nodesel) :
+            try:
+                inv_proba = float(nodesel.split('_')[-1])
+            except:
+                inv_proba = 0
+            comp = OracleNodeSelectorAbdel('optimal_plunger', optsol=0,inv_proba=inv_proba)
+            oracles.append(comp)
         
-        model.includeNodesel(oracle_estimator_trained, "gnn_trained", 'testing',100, 100)
-    
-    if "gnn_untrained" in nodesels:
-        from node_selection.recorders import CompFeaturizer, LPFeatureRecorder
-        from node_selection.node_selectors.oracle_selectors import OracleNodeSelectorEstimator
-        from learning.train import normalize_graph
-        
-        comp_featurizer = CompFeaturizer(normalizor=normalize_graph if normalize else None)
-        oracle_estimator_untrained = OracleNodeSelectorEstimator(problem,
-                                                       comp_featurizer,
-                                                       DEVICE=device,
-                                                       record_fpath="decisions_gnn_untrained.csv",
-                                                       use_trained_gnn=False)
-        
-        model.includeNodesel(oracle_estimator_untrained, "gnn_untrained", 'testing',100, 100)
-        
-    #creating appropriate oracle 
-    if 'oracle_0' in nodesels:
-        from node_selection.node_selectors.oracle_selectors import OracleNodeSelectorAbdel
-        oracle_0 = OracleNodeSelectorAbdel("optimal_plunger", inv_proba=0)
-        model.includeNodesel(oracle_0, 'oracle_0', 'testing',100, 100)
-        
-    def find_2d(matrix, elem):
-        for idx, array in enumerate(matrix):
-            if elem in array and float(array[1]) > 0:
-                return idx, array
-        return -1, ""
-    
-    oracle_idx, oracle_params = find_2d([ n.split("_") for n in nodesels ], 'oracle')
-    if oracle_idx != -1:
-        from node_selection.node_selectors.oracle_selectors import OracleNodeSelectorAbdel
-        inv_proba = 0 if len(oracle_params) == 1 else float(oracle_params[1]) 
-        oracle = OracleNodeSelectorAbdel("optimal_plunger", inv_proba=inv_proba)
-        model.includeNodesel(oracle, nodesels[oracle_idx], 'testing',100, 100)
+        try:
+            model.includeNodesel(comp, nodesel, 'testing', 100, 100)
+        except:
+            '' #tying to include a nodesel already present
         
     
     
@@ -94,12 +76,7 @@ def record_stats(nodesels, instances, problem, normalize=True, device='cpu', ver
         
         
         #setup oracles
-        for idx,o in enumerate([oracle_estimator_trained, oracle_estimator_untrained, oracle_0, oracle]):
-            if o != None:
-                if idx < 2 : #estimators
-                    o.set_LP_feature_recorder(LPFeatureRecorder(model.getVars(), model.getConss()))
-                o.setOptsol(optsol)
-    
+        setup_oracles(model, optsol, oracles)
             
         if verbose:    
             print("----------------------------")
@@ -109,8 +86,6 @@ def record_stats(nodesels, instances, problem, normalize=True, device='cpu', ver
             
             model.freeTransform()
             model.readProblem(instance)
-            print("------------------------")
-            
             #canceL all otheer nodesels, WORKS
             for other in nodesels:
                     model.setNodeselPriority(other, 100)
@@ -119,14 +94,6 @@ def record_stats(nodesels, instances, problem, normalize=True, device='cpu', ver
             model.setNodeselPriority(nodesel, 536870911)
             
             model.optimize()
-            if verbose:
-                print(f"  Nodeselector : {nodesel}")
-                print(f"    # of processed nodes : {model.getNNodes()} \n")
-                print(f"    Time                 : {model.getSolvingTime()} \n")
-            if nodesel == "gnn_trained":
-                if verbose:
-                    print(f"fe time : {oracle_estimator_trained.fe_time}")
-                    print(f"inference time : {oracle_estimator_trained.inference_time}")
             
             with open(f"nnodes_{problem}_{nodesel}.csv", "a+") as f:
                 f.write(f"{model.getNNodes()},")
@@ -142,7 +109,6 @@ def record_stats(nodesels, instances, problem, normalize=True, device='cpu', ver
 
 
 def display_stats(nodesels, problem, alternative_stdout=None):
-    from scipy.stats import entropy
     import matplotlib.pyplot as plt
     import numpy as np
         
@@ -151,6 +117,16 @@ def display_stats(nodesels, problem, alternative_stdout=None):
     for nodesel in nodesels:
          nnodes = np.genfromtxt(f"nnodes_{problem}_{nodesel}.csv", delimiter=",")[:-1]
          times = np.genfromtxt(f"times_{problem}_{nodesel}.csv", delimiter=",")[:-1]
+         
+         if nodesel in ['gnn_trained', 'gnn_untrained']:
+             decisions = np.genfromtxt(f'decisions_{nodesel}.csv', delimiter=',')
+             plt.figure()
+             plt.title(f'decisions {nodesel}')
+             plt.hist(decisions[:,0])
+             plt.savefig(f'decisions_{nodesel}.png')
+             print(f'Accuracy in {nodesel} : {np.mean( np.round(decisions[:,0]) == 0.5*decisions[:,1]+0.5  ):0.3f}' )
+         
+             
          print(f"  {nodesel} ")
          print(f"    Number of instances solved    : {len(nnodes)}")
          print(f"    Mean number of node created   : {np.mean(nnodes):.2f}")
@@ -159,30 +135,6 @@ def display_stats(nodesels, problem, alternative_stdout=None):
          #print(f"    Median solving time           : {np.median(times):.2f}")
          print("--------------------------")
          
-    if 'gnn_trained' in nodesels:
-        decisions_gnn_trained = np.genfromtxt("decisions_gnn_trained.csv", delimiter=",")
-        plt.figure()
-        plt.title("decisions gnn trained")
-        plt.hist(decisions_gnn_trained[:,0])
-        plt.savefig("decisions_gnn_trained.png")
-        print(f"Accuracy in trained GNN : {np.mean( np.round(decisions_gnn_trained[:,0]) == 0.5*decisions_gnn_trained[:,1] + 0.5 ):0.3f}"  )
-        
-    if 'gnn_untrained' in nodesels:
-        decisions_gnn_untrained = np.genfromtxt("decisions_gnn_untrained.csv", delimiter=",")
-        plt.figure()
-        plt.title("decisions gnn untrained")
-        plt.hist(decisions_gnn_untrained[:,0])
-        plt.savefig("decisions_gnn_untrained.png")
-        print(f"Accuracy in untrained GNN : {np.mean( np.round(decisions_gnn_untrained[:,0]) == 0.5*decisions_gnn_untrained[:,1]+0.5  ):0.3f}"  )
-    
-      
-    if 'random' in nodesels:
-        decisions_rand = np.genfromtxt("decisions_rand.csv", delimiter=",")[:-1]
-        plt.figure()
-        plt.title("decisions rand")
-        plt.hist(decisions_rand)
-        plt.savefig("decisions_rand.png")
-    
     
     if alternative_stdout != None:
         sys.stdout = alternative_stdout
