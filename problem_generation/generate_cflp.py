74a2dfec622d1025bf0ce0b61056586c8650faff
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2022-02-16 12:16:58 -0500

    Update generate_cflp.py : add id to erach instance to avoid duplicatas

--- problem_generation/generate_cflp.py
+++ problem_generation/generate_cflp.py
@@ -97,7 +97,8 @@ def generate_instances(start_seed, end_seed, min_n, max_n, lp_dir, solveInstance
         n_facility = np.random.randint(min_n, max_n+1)
         ratio = 5
         rng = np.random.RandomState(seed)
-        instance_name = f'n_customer={n_customer}_n_facility={n_facility}_ratio={ratio}'
+        instance_id = np.random.rand()*100
+        instance_name = f'n_customer={n_customer}_n_facility={n_facility}_ratio={ratio}_id_{instance_id:0.2f}'
         instance_path = lp_dir +  "/" + instance_name
         generate_capacited_facility_location(rng, instance_path + ".lp", n_customer, n_facility, ratio)
         
