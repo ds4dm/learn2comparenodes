d4375008509ed40ebf11c06c097e3e9b2dc77e20
Abdel Ghani Labassi <abdel.ghani.labassi@umontreal.ca>
2022-04-03 12:08:21 -0400

    Update recorders.py

--- node_selection/recorders.py
+++ node_selection/recorders.py
@@ -400,30 +400,30 @@ class LPFeatureRecorder():
     
     def _get_feature_cons(self, model, cons):
 
-		try:
+	try:
 
-		  cons_n = str(cons)
+	  cons_n = str(cons)
 
-		  if re.match('flow', cons_n):
-			 rhs = model.getRhs(cons)
-			 leq = 0
-			 eq = 1
-			 geq = 0
-		  elif re.match('arc', cons_n):
-			 rhs = 0
-			 leq = eq =  1
-			 geq = 0
-		  else:
-			 rhs = model.getRhs(cons)
-			 leq = eq = 1
-			 geq = 0
-		except:
-		  'logicor no repr'
-		  rhs = 0
-		  leq = eq = 1
-		  geq = 0
+	  if re.match('flow', cons_n):
+		 rhs = model.getRhs(cons)
+		 leq = 0
+		 eq = 1
+		 geq = 0
+	  elif re.match('arc', cons_n):
+		 rhs = 0
+		 leq = eq =  1
+		 geq = 0
+	  else:
+		 rhs = model.getRhs(cons)
+		 leq = eq = 1
+		 geq = 0
+	except:
+	  'logicor no repr'
+	  rhs = 0
+	  leq = eq = 1
+	  geq = 0
 
-		return torch.tensor([ rhs, leq, eq, geq ], device=self.device).float()
+	return torch.tensor([ rhs, leq, eq, geq ], device=self.device).float()
 
     
     def _get_feature_var(self, model, var):
