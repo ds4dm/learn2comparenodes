1dd082b5316612fa8d88afe8cd92c7c08f85266b
Abdel Ghani Labassi <abdel.ghani.labassi@gmail.com>
2022-04-05 22:31:19 -0400

    Update maxsat.py : debug generator

--- problem_generation/maxsat.py
+++ problem_generation/maxsat.py
@@ -13,21 +13,20 @@ import pyscipopt.scip as sp
 #https://computational-sustainability.cis.cornell.edu/cpaior2013/pdfs/ansotegui.pdf
 def get_clauses(rng,H,W, n_piece, n_obstacle):
 
-    pieces = [(rng.randint(1,H), rng.randint(1,W)) for _ in range(n_piece) ]
+    pieces = [(rng.randint(1,H+1), rng.randint(1,W+1)) for _ in range(n_piece) ] + [(1,1) ]*n_obstacle
     
     #Generate obstacles, aka filled pieces
-    obstacles = []
-    while(len(obstacles) != n_obstacle):
-        candidat = (rng.randint(0,H+1), rng.randint(0,W+1))
-        if candidat not in obstacles:
-            obstacles.append(candidat)
+    obstacle_pos = []
+    while(len(obstacle_pos) != n_obstacle):
+        candidat = (rng.randint(0,H), rng.randint(0,W))
+        if candidat not in obstacle_pos:
+            obstacle_pos.append(candidat)
             
-    piece_and_obstacles = pieces + obstacles
 
         
     
     #Soft clauses
-    clauses = [(f'x{k}', piece[0]*piece[1]) for k,piece in enumerate(pieces)]
+    clauses = [(f'x{k}', piece[0]*piece[1]) for k,piece in enumerate(pieces[:n_piece])]
     
     
 
@@ -35,43 +34,51 @@ def get_clauses(rng,H,W, n_piece, n_obstacle):
     #No  two times same row/colomn per piece
     for k,piece in enumerate(pieces):
         h,w = piece
-        clauses.append((f'-x{k},' + ','.join([ f'r{k}_{h+i}' for i in range(0,H-h+1)  ]),np.inf))
+        clauses.append((f'-x{k},' + ','.join([ f'r{i}_{k}' for i in range(0,H-h+1)  ]),np.inf))
 
-        for i in range(h, H+1):
-            for j in range(i+1, H+1):
-                clauses.append((f'-x{k},-r{k}_{i},-r{k}_{j}',np.inf))
+        for i in range(0, H-h+1):
+            for j in range(i+1, H-h+1):
+                clauses.append((f'-x{k},-r{i}_{k},-r{j}_{k}',np.inf))
 
         
-        clauses.append((f'-x{k},' + ','.join([ f'c{k}_{i}' for i in range(0,W-w+1)  ]), np.inf))
+        clauses.append((f'-x{k},' + ','.join([ f'c{j}_{k}' for j in range(0,W-w+1)  ]), np.inf))
 
         for i in range(0, W-w+1):
             for j in range(i+1,W-w+1):
-                clauses.append((f'-x{k},-c{k}_{i},-c{k}_{j}',np.inf))
+                clauses.append((f'-x{k},-c{i}_{k},-c{j}_{k}',np.inf))
 
     #Place obstacles
-    for k,piece in enumerate(obstacles):
-        i,j = piece
-        reajusted_k = len(pieces) + k
-        clauses.append((f'r{reajusted_k}_{i}', np.inf))
-        clauses.append((f'c{reajusted_k}_{j}', np.inf))
+    for k,pos in enumerate(obstacle_pos):
+        i,j = pos
+        reajusted_k = n_piece + k
         clauses.append((f'x{reajusted_k}', np.inf))
+        clauses.append((f'r{i}_{reajusted_k}', np.inf))
+        clauses.append((f'c{j}_{reajusted_k}', np.inf))
 
-    #No overlapping between pieces(and obstacles)
-    for s,piece1 in enumerate( piece_and_obstacles ):
+
+    # #No overlapping between pieces(and obstacles)
+    
+    
+    cl_s  = []
+    for s,piece1 in enumerate( pieces ):
         h,w = piece1
 
-        for k in range(s+1, len( piece_and_obstacles)):
-
-            for i in range(h, H+1):
-                for j in range(w, W+1):
-                    for l in range(i-h+1, i+1):
-                        for m in range(j,j+w):
-                            clauses.append((f'-r{s}_{i},-c{s}_{j},-r{k}_{l},-c{k}_{m}', np.inf))
-
+        for k,piece2 in enumerate(pieces):
+            if k != s:
+    
+                for i in range(0, H-h+1):
+                    
+                    for j in range(0, W-w+1):
+                        for l in range(i, i+h):
+                            for m in range(j, j+w):
+    
+                                cl = (f'-r{i}_{s},-c{j}_{s},-r{l}_{k},-c{m}_{k}',np.inf)
+                                cl_s.append(cl)
+            
 
     
 
-    return clauses
+    return clauses + cl_s
 
 
 
@@ -90,11 +97,10 @@ def write_lp(clauses, filename):
         '''
     
     var_names = dict() #maps var appearing in order i in clauses (whatever the name ) to y_i
-    counter = 0
 
     with open(filename, 'w') as file:
         file.write("maximize\nOBJ:")
-        file.write("".join([f" +{clause[1]}c_{idx}" for idx,clause in enumerate(clauses) if clause[1] < np.inf ]))
+        file.write("".join([f" +{clause[1]}cl_{idx}" for idx,clause in enumerate(clauses) if clause[1] < np.inf ]))
 
         
         
@@ -110,18 +116,16 @@ def write_lp(clauses, filename):
                 if var != '':
                     if var[0] == '-':
                         if var[1:] not in var_names:
-                            var_names[var[1:]] = f"y_{counter}"
-                            counter += 1
+                            var_names[var[1:]] =  var[1:]
                         neg_varrs.append(var_names[var[1:]])
                         
                     else:
                         if var[0:] not in var_names:
-                            var_names[var[0:]] = f"y_{counter}"
-                            counter += 1
+                            var_names[var[0:]] = var[0:]
                         pos_varrs.append(var_names[var[0:]])
                         
                             
-            last_part = f' +c_{idx} <= {len(neg_varrs)} \n' if clause[1] < np.inf else f' <= {len(neg_varrs) - 1} \n'           
+            last_part = f' +cl_{idx} <= {len(neg_varrs)} \n' if clause[1] < np.inf else f' <= {len(neg_varrs) - 1} \n'           
                     
             
             file.write(f"clause_{idx}:" + ''.join([ f" -{yi}" for yi in pos_varrs]) + ''.join([ f" +{yi}" for yi in neg_varrs]) + last_part) 
@@ -134,7 +138,7 @@ def write_lp(clauses, filename):
         file.write("\nBinaries\n")
         for idx in range(len(clauses)):
             if clauses[idx][1] < np.inf:
-                file.write(f" c_{idx}")
+                file.write(f" cl_{idx}")
             
         for var_name in var_names.keys():
             file.write(f" {var_names[var_name]}")
@@ -146,7 +150,7 @@ def write_lp(clauses, filename):
 seed = 0
 rng = np.random.RandomState(seed)
         
-clauses = get_clauses(rng,10,10,100,0) 
+clauses = get_clauses(rng,7,7,10,0) 
 file_name = '34.lp'
 
 
@@ -154,3 +158,4 @@ write_lp(clauses, file_name)
 m = sp.Model()
 m.readProblem(file_name)
 m.optimize()
+m.writeBestSol()
